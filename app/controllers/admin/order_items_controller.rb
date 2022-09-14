0d92a02f1210aed79a98e61fb31e5656c68d1e56
EC2 Default User <10471922+hiromulxs@users.noreply.github.com>
2022-09-14 11:57:46 +0000

    [Add]製作ステータス

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,8 +1,8 @@
 class Admin::OrderItemsController < ApplicationController
   def update
-    @order = Order.find(params[:id])
-    @order.order_item.update(order_item_params)
-    redirect_to admin_order_path
+    @order_item = OrderItem.find(params[:order_item][:order_item_id]) #いまいち理解不足質問して確認する
+    @order_item.update(order_item_params)
+    redirect_to admin_order_path(@order_item.order.id)
   end
 
   private
